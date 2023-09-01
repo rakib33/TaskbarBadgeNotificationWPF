e88e0c5699701a0b2bcd2e547c294a2d48864101
rakibul islam <islam.rakibul@bjitgroup.com>
2023-09-01 16:30:00 +0600

    [create windows/wpf project to display badge on taskbar app icon]

---
+++ BadgeNotification/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BadgeNotification
+{
+
+    public partial class MainWindow : Window
+    {
+        NotifyIconHandler notifyIconHandler;
+        //http://10rem.net/blog/2010/05/29/creating-dynamic-windows-7-taskbar-overlay-icons
+        public MainWindow()
+        {
+            InitializeComponent();
+            string iconOverlayText = "1";
+            notifyIconHandler = new NotifyIconHandler();
+            notifyIconHandler.InitializeNotifyIcon();
+        }
+
+        private void UpdateCount_Click(object sender, RoutedEventArgs e)
+        {
+            int iconWidth = 20;
+            int iconHeight = 20;
+
+            string countText = EnteredCount.Text.Trim();
+
+            RenderTargetBitmap bmp =
+            new RenderTargetBitmap(iconWidth, iconHeight, 96, 96, PixelFormats.Default);
+            ContentControl root = new ContentControl();
+            root.ContentTemplate = ((DataTemplate)Resources["OverlayIcon"]);
+            root.Content = countText;
+            root.Arrange(new Rect(0, 0, iconWidth, iconHeight));
+            bmp.Render(root);
+            TaskbarItemInfo.Overlay = (ImageSource)bmp;
+
+            //display notification icon
+            notifyIconHandler.BadgeShowTooltip();
+            notifyIconHandler.ShowBadgeNotification();
+        }
+
+        private void RemoveCount_Click(object sender, RoutedEventArgs e)
+        {
+            TaskbarItemInfo.Overlay = null;
+            //hide notify icon
+            notifyIconHandler.BadgeHideTooltip();
+            notifyIconHandler.HideBadgeNotification();
+        }
+    }
+}
---
+++ BadgeNotification/NotifyIconHandler.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Globalization;
+using Forms = System.Windows.Forms;
+
+namespace BadgeNotification
+{
+    public class NotifyIconHandler
+    {
+        private Forms.NotifyIcon _notifyIcon;
+       public void InitializeNotifyIcon()
+        {
+            _notifyIcon = new Forms.NotifyIcon();
+            _notifyIcon.Icon = new System.Drawing.Icon("logo.ico");
+            _notifyIcon.Visible = true;
+        }
+        // Public static method to access the single instance of the class     
+
+        public void BadgeHideTooltip()
+        {
+            _notifyIcon.Icon = new System.Drawing.Icon("logo.ico");
+            _notifyIcon.Text = "Badge Hide";
+
+        }
+        public void BadgeShowTooltip()
+        {
+            _notifyIcon.Icon = new System.Drawing.Icon("logo.ico");
+            _notifyIcon.Text = "Badge Show";           
+        }
+
+
+        public void ShowBadgeNotification()
+        {       
+            _notifyIcon.ShowBalloonTip(100, "Badge", "Show Badge", Forms.ToolTipIcon.Info);
+        }
+        public void HideBadgeNotification()
+        {
+          _notifyIcon.ShowBalloonTip(100, "Badge", "Hide Badge", Forms.ToolTipIcon.Warning);
+        }
+        public void DisposeNotifyIcon()
+        {
+            _notifyIcon.Dispose();
+        }
+    }
+}
